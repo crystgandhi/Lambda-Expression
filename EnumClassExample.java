28d62a69f0c8cf35c9fa593348589a5b90d69783
crystgandhi <sam@example.comcrystgandhi@gmail.com>
2023-05-13 14:27:56 -0400

    my

---
+++ EnumClassExample.java
@@ -0,0 +1,14 @@
+
+public class EnumClassExample {
+	enum Level {
+	    LOW,
+	    MEDIUM,
+	    HIGH
+	  }
+
+	public static void main(String[] args) {
+		 Level myVar = Level.HIGH; 
+		    System.out.println(myVar);	    
+	}     
+
+}
